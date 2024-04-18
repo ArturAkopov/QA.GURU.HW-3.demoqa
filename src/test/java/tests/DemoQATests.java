a142e9ec14292822ec1567589270161e18dfb3c0
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-04-18 20:47:58 +0300

    HW-3

---
+++ src/test/java/tests/DemoQATests.java
@@ -0,0 +1,46 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DemoQATests {
+
+    @BeforeAll
+    static void beforeAll(){
+          Configuration.baseUrl = "https://demoqa.com/";
+          Configuration.pageLoadStrategy ="eager";
+          Configuration.holdBrowserOpen=true;
+    }
+
+    @AfterEach
+    void closeWebDriver(){
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void fillFormTest() {
+        open("automation-practice-form");
+        $("#firstName").setValue("Artur");
+        $("#lastName").setValue("King");
+        $("#userEmail").setValue("king@yandex.ru");
+        $("#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label").click();
+        $("#userNumber").setValue("9611111111");
+        $("#dateOfBirthInput").click();
+        $("#dateOfBirthInput").setValue("04 Nov 1992");
+        $("#subjectsInput").setValue("Comp").pressEnter();
+        $("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label").click();
+        $("#uploadPicture").uploadFile(new File("src/test/java/tests/Example.jpg"));
+        $("#currentAddress").setValue("Castle Camelot").pressEnter();
+        $("#react-select-3-input").setValue("ncr").pressEnter();
+        $("#react-select-4-input").setValue("del").pressEnter();
+        $("#submit").click();
+    }
+}
