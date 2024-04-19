943533aa6abb2edcd61e6fadb20da651e6087196
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-04-19 09:12:21 +0300

    Удален лишний импорт

--- src/test/java/tests/DemoQATests.java
+++ src/test/java/tests/DemoQATests.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
